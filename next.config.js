e99fb2f5ed7b10bed1000366f1d6d20c7e348a58
Your Name~ <you@example.com>
2023-10-02 12:28:31 +0500

    changes7

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'ezey-blog-nextjs.vercel.app/:path*',
+          },
+        ]
+    },
+};
