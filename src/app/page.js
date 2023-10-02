666aced1e0baf9086a2de5b2aa4f2c02125eda30
Your Name~ <you@example.com>
2023-10-02 12:16:22 +0500

    changes5

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import BlogCard from './components/blogCard/BlogCard'
 
-const getBlogs = async () => {
-  let res = await fetch(`${process.env.DOMAIN_NAME}/api/blog`, {cache: 'no-store'});
-  return res.json();
-}
+// const getBlogs = async () => {
+//   let res = await fetch(`${process.env.DOMAIN_NAME}/api/blog`, {cache: 'no-store'});
+//   return res.json();
+// }
 
 const HomePage = async () => {
-  const blogData = await getBlogs();
+  const blogData = [];
+  // const blogData = await getBlogs();
   return (
     <div>
       <div className='w-[80%] mx-auto mt-10 flex flex-col items-center'>
